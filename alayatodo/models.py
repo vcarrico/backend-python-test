6492414216b783fac0a3f8b86bc6fd269cae0717
victor carrico <victor@vinta.com.br>
2018-12-16 14:44:09 -0300

    TASK-2: Todo migrations - adding completed column

--- alayatodo/models.py
+++ alayatodo/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), unique=True, nullable=False)
+    username = db.Column(db.String(255), nullable=False)
     password = db.Column(PasswordType(
         schemes=['pbkdf2_sha512', ]), nullable=False)
     todos = db.relationship('Todo', backref='person', lazy=True)
@@ -28,6 +28,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(255))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return "To-Do <User.id {}>: {}".format(self.user_id, self.description)
